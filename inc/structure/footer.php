f9a12fa74e2ee0e842e2c394d10d174ac31dbd17
Mladjo <mladen@milentijevic.com>
2015-02-24 18:44:20 +0100

    init

--- inc/structure/footer.php
+++ inc/structure/footer.php
@@ -14,8 +14,11 @@ if ( ! function_exists( 'creativefocus_credit' ) ) {
 	function creativefocus_credit() {
 		?>
 		<div class="site-info">
-			<?php echo esc_html( apply_filters( 'creativefocus_copyright_text', $content = '&copy; ' . get_bloginfo( 'name' ) . ' 2006 - ' . date( 'Y' ) ) ); ?>
+			<?php echo esc_html( apply_filters( 'creativefocus_copyright_text', $content = '&copy; ' . get_bloginfo( 'name' ) . ' ' . date( 'Y' ) ) ); ?>
 			<?php if ( apply_filters( 'creativefocus_credit_link', true ) ) { ?>
+			<br /> 			
+                            <?php printf( __( 'Theme: %1$s by %2$s.', 'creativefocus' ), 'creativefocus', '<a href="http://milentijevic.com/" rel="designer">Mladjo</a>' ); ?>
+
 			<?php } ?>
 		</div><!-- .site-info -->
 		<?php
